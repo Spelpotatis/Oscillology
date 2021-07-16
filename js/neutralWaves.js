159502d67896f279150b8264055c04b51342aa00
Diamboy211 <51283163+Diamboy211@users.noreply.github.com>
2021-07-16 14:38:26 +0700

    lmao nice bug

--- js/neutralWaves.js
+++ js/neutralWaves.js
@@ -1,7 +1,7 @@
 function buyNeutralWave() {
-    if (game.number >= game.neutralCost){
-        game.number = game.number.sub(game.neutralCost)
-        game.neutralWaves = game.neutralWaves.add(1)
-        game.neutralCost = game.neutralCost.mul(game.neutralScaling)
+    if (game.number.gte(game.neutralCost)) {
+        game.number = game.number.sub(game.neutralCost);
+        game.neutralWaves = game.neutralWaves.add(1);
+        game.neutralCost = game.neutralCost.mul(game.neutralScaling);
     }
 }
